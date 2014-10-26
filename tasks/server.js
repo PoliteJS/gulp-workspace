3ca514b13cedb15cf84bc7576cc7f861ae8ced76
thepeg <marco.pegoraro@gmail.com>
2014-10-26 16:43:14 +0100

    added support for background processes

--- tasks/server.js
+++ tasks/server.js
@@ -1,37 +1,86 @@
 var childProcess = require('child_process');
+var open = require("open");
 
 exports.start = function(gulp, config) {
 
 	gulp.task('wks-server', function (done) {
+		startServer(done);
+	});
 
-		var processArgs;
-		var serverArgs = '';
-		var serverExec = 'foo';
+	gulp.task('wks-server-show', function (done) {
+		var port = startServer(done);
+		open('http://localhost:' + port);
+	});
 
-		try {
-			processArgs = require('yargs').argv;
-		} catch(e) {
-			processArgs = {};
-		};
-
-		if (processArgs.p) {
-			serverArgs += ' -p ' + processArgs.p;
-		}
-		if (processArgs.r) {
-			serverArgs += ' -w build/prod';
-		} else {
-			serverArgs += ' -w build/dev';
-		}
-
-		serverExec = 'node node_modules/gulp-workspace/debug-server.js' + serverArgs;
-
-		var server = childProcess.exec(serverExec, function() {});
-        
-        server.stdout.on('data', function(data) {
-            console.log(data);
-        });
-        
-        server.on('exit', done);
+	gulp.task('wks-start', function (done) {
+		var args = require('yargs').argv;
+		startServer(function() {
+	    	console.log('development server has crashed');
+	    	done();
+	    });
+	    startWatch(function() {
+	    	console.log('watch proces has crashed');
+	    	done();
+	    });
+	    if (args.t) {
+	    	startTdd(function() {
+		    	console.log('tdd proces has crashed');
+		    	done();
+		    });
+	    }
 	});
 
 };
+
+function startServer(done) {
+
+	var usingPort = '8080';
+
+	var processArgs;
+	var serverArgs = '';
+	var serverExec = 'foo';
+
+	try {
+		processArgs = require('yargs').argv;
+	} catch(e) {
+		processArgs = {};
+	};
+
+	if (processArgs.p) {
+		serverArgs += ' -p ' + processArgs.p;
+		usingPort = processArgs.p;
+	}
+	if (processArgs.r) {
+		serverArgs += ' -w build/prod';
+	} else {
+		serverArgs += ' -w build/dev';
+	}
+
+	serverExec = 'node node_modules/gulp-workspace/debug-server.js' + serverArgs;
+
+	var server = childProcess.exec(serverExec, function() {});
+    
+    server.stdout.on('data', function(data) {
+        console.log(data);
+    });
+    
+    server.on('exit', done);
+
+    return usingPort;
+}
+
+function startWatch(done) {
+	var watch = childProcess.exec('gulp watch', function() {});
+    watch.stdout.on('data', function(data) {
+        console.log(data);
+    });
+    watch.on('exit', done);
+}
+
+function startTdd(done) {
+	var watch = childProcess.exec('gulp tdd', function() {});
+    watch.stdout.on('data', function(data) {
+        console.log(data);
+    });
+    watch.on('exit', done);
+}
